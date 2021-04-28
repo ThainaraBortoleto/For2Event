12e9b16486887ff23b87441c888fc4a4542f2942
root <root@DESKTOP-SSC1QKH.localdomain>
2021-04-28 10:49:49 -0300

    ajustando ambiente

---
+++ backend/src/api/main/index.js
@@ -0,0 +1,5 @@
+const { Router } = require('express');
+const router = Router();
+
+router.get('/', require('./main-get'));
+module.exports = router;
---
+++ backend/src/api/posts/post-create-post.js
@@ -0,0 +1,15 @@
+const Post = require('./../../models/post');
+const slugify = require('slugify');
+
+module.exports = async (req, res) => {
+   
+    req.body.slug = slugify(req.body.title);
+    const post = new Post(req.body);
+    console.log(post);
+    try {
+        const saved = await post.save();
+        return res.status(201).json({ post: saved });
+    } catch(err) {
+        return res.status(500).json({err});
+    }
+}
